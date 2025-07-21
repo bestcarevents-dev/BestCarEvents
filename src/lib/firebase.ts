7c3092d6890a43ba23bd4d4691eba00caf86bc56
iWrity Development <iwritydevelopment@gmail.com>
2025-07-21 21:07:04 +0000

    firebase change

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAtn7-A8IrBWZzU7OpPa2QolSJZq_zPTp8",
-  authDomain: "bestcarevents2025.firebaseapp.com",
-  projectId: "bestcarevents2025",
-  storageBucket: "bestcarevents2025.firebasestorage.app",
-  messagingSenderId: "228810152023",
-  appId: "1:228810152023:web:01a903d23e9f270d05eb12",
-  measurementId: "G-P37KBEVQDV"
-};
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+      // Import the functions you need from the SDKs you need
+      import { initializeApp } from "firebase/app";
+      // TODO: Add SDKs for Firebase products that you want to use
+      // https://firebase.google.com/docs/web/setup#available-libraries
+      
+      // Your web app's Firebase configuration
+      const firebaseConfig = {
+        apiKey: "AIzaSyDLAqY4ON29pDWHuor8HiTYnxTY-Ekr_bc",
+        authDomain: "bestcarevents-dev.firebaseapp.com",
+        projectId: "bestcarevents-dev",
+        storageBucket: "bestcarevents-dev.firebasestorage.app",
+        messagingSenderId: "653477912519",
+        appId: "1:653477912519:web:77ef3c7b6617883efe03e2",
+        measurementId: "G-5ZGKP6TS4C"
+      };
+      
+      // Initialize Firebase
+      export const app = initializeApp(firebaseConfig);
