bcf339ef251cf8116faae5a3a36d6b1bca83e9e7
Huzaifa8194 <huzaifa8195@gmail.com>
2025-09-06 13:51:01 +0500

    lint fix 2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { app } from "@/lib/firebase";
